53b9f401e87f671f3a03a2f7a3bdd1a8f433697a
jgiwinski <juliaiwinski@gmail.com>
2021-05-08 18:43:49 -0700

    Add responsiveness to nominee container

--- src/components/Nominee/Nominee.js
+++ src/components/Nominee/Nominee.js
@@ -6,8 +6,8 @@ const Nominee = ({ title, year, id, removeFilm }) => {
     
     return (
         <div className="nominee-container">
-            <h2 className="movie-title">{title}</h2>
-            <h2 className="movie-year">{year}</h2>
+            <h2 className="nominee-title">{title}</h2>
+            <h2 className="nominee-year">{year}</h2>
             <button className="remove-btn" id={id} onClick={event => removeFilm(event)}>Remove</button>
         </div>
     )

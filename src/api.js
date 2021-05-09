d979d5faa054c685b0ddad0bef6ccf7d087b4bfb
jgiwinski <juliaiwinski@gmail.com>
2021-05-08 20:12:55 -0700

    Add env file for api key

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
+const api_key = process.env.REACT_APP_API_KEY;
+
 export const getData = ( title ) => {
-    return fetch(`http://www.omdbapi.com/?apikey=ff1a2f36&s=${title}`)
+    return fetch(`http://www.omdbapi.com/?apikey=${api_key}&s=${title}`)
       .then(response => {
         if (response.ok) {
           return response.json()
